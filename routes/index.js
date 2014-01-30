2e3b145d6b363ac906d8d9b50c593a321a4f13c6
unknown <peltz@SEA-L-PELTZ.olympus.F5Net.com>
2014-01-30 12:29:43 -0800

    First commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+//var ctrl = require('../app_server/controllers/main');
+
+//module.exports = function (app) {
+//  app.get('/', ctrl.index);
+//};
+
+module.exports = function(app) {
+  require('./main') (app);
+  };
